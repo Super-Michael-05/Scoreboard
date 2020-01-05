7f4279205ec68c0e981c78d3f9644a5a19d95540
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2020-01-05 19:45:35 +0200

    update 05.01.2020 19:38

---
+++ High Score Table-code/Assets/HighscoreTable/Scripts/HighscoreTable.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey.Utils;
+
+public class HighscoreTable : MonoBehaviour {
+
+    private Transform entryContainer;
+    private Transform entryTemplate;
+    private List<Transform> highscoreEntryTransformList;
+
+    private void Awake() {
+        entryContainer = transform.Find("highscoreEntryContainer");
+        entryTemplate = entryContainer.Find("highscoreEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+        if (highscores == null) {
+
+            Debug.Log("Initializing table with default values...");
+            AddHighscoreEntry(1000000, "MAK");
+
+            jsonString = PlayerPrefs.GetString("highscoreTable");
+            highscores = JsonUtility.FromJson<Highscores>(jsonString);
+        }
+
+        RefreshHighscoreTable();
+    }
+    private void RefreshHighscoreTable() {
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+        for (int i = 0; i < highscores.highscoreEntryList.Count; i++) {
+            for (int j = i + 1; j < highscores.highscoreEntryList.Count; j++) {
+                if (highscores.highscoreEntryList[j].score > highscores.highscoreEntryList[i].score) {
+
+                    HighscoreEntry tmp = highscores.highscoreEntryList[i];
+                    highscores.highscoreEntryList[i] = highscores.highscoreEntryList[j];
+                    highscores.highscoreEntryList[j] = tmp;
+                }
+            }
+        }
+        if (highscoreEntryTransformList != null) {
+            foreach (Transform highscoreEntryTransform in highscoreEntryTransformList) {
+                Destroy(highscoreEntryTransform.gameObject);
+            }
+        }
+        highscoreEntryTransformList = new List<Transform>();
+        foreach (HighscoreEntry highscoreEntry in highscores.highscoreEntryList) {
+            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+        }
+    }
+    private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList) {
+        float templateHeight = 31f;
+        Transform entryTransform = Instantiate(entryTemplate, container);
+        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+        entryTransform.gameObject.SetActive(true);
+
+        int rank = transformList.Count + 1;
+        string rankString;
+        switch (rank) {
+        default:
+            rankString = rank + "TH"; break;
+
+        case 1: rankString = "1ST"; break;
+        case 2: rankString = "2ND"; break;
+        case 3: rankString = "3RD"; break;
+        }
+
+        entryTransform.Find("posText").GetComponent<Text>().text = rankString;
+
+        int score = highscoreEntry.score;
+
+        entryTransform.Find("scoreText").GetComponent<Text>().text = score.ToString();
+
+        string name = highscoreEntry.name;
+        entryTransform.Find("nameText").GetComponent<Text>().text = name;
+
+        entryTransform.Find("background").gameObject.SetActive(rank % 2 == 1);
+        
+        if (rank == 1) {
+            entryTransform.Find("posText").GetComponent<Text>().color = Color.green;
+            entryTransform.Find("scoreText").GetComponent<Text>().color = Color.green;
+            entryTransform.Find("nameText").GetComponent<Text>().color = Color.green;
+        }
+        switch (rank) {
+        default:
+            entryTransform.Find("trophy").gameObject.SetActive(false);
+            break;
+        case 1:
+            entryTransform.Find("trophy").GetComponent<Image>().color = UtilsClass.GetColorFromString("FFD200");
+            break;
+        case 2:
+            entryTransform.Find("trophy").GetComponent<Image>().color = UtilsClass.GetColorFromString("C6C6C6");
+            break;
+        case 3:
+            entryTransform.Find("trophy").GetComponent<Image>().color = UtilsClass.GetColorFromString("B76F56");
+            break;
+
+        }
+        transformList.Add(entryTransform);
+    }
+    public void AddHighscoreEntry(int score, string name) {
+
+        HighscoreEntry highscoreEntry = new HighscoreEntry { score = score, name = name };
+
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+        if (highscores == null) {
+
+            highscores = new Highscores() {
+                highscoreEntryList = new List<HighscoreEntry>()
+            };
+        }
+
+        highscores.highscoreEntryList.Add(highscoreEntry);
+
+        string json = JsonUtility.ToJson(highscores);
+        PlayerPrefs.SetString("highscoreTable", json);
+        PlayerPrefs.Save();
+
+        RefreshHighscoreTable();
+    }
+    private class Highscores {
+        public List<HighscoreEntry> highscoreEntryList;
+    }
+    [System.Serializable] 
+    private class HighscoreEntry {
+        public int score;
+        public string name;
+    }
+}
---
+++ High Score Table-code/Assets/HighscoreTable/Scripts/UI_Testing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using CodeMonkey;
+
+public class UI_Testing : MonoBehaviour {
+
+    [SerializeField] private HighscoreTable highscoreTable;
+
+    private void Start() {
+        transform.Find("submitScoreBtn").GetComponent<Button_UI>().ClickFunc = () => {
+            UI_Blocker.Show_Static();
+
+            UI_InputWindow.Show_Static("Score", 0, () => {
+
+                UI_Blocker.Hide_Static();
+            }, (int score) => {
+
+                UI_InputWindow.Show_Static("Player Name", "", "ABCDEFGIJKLMNOPQRSTUVXYWZ", 3, () => {
+
+                    UI_Blocker.Hide_Static();
+                }, (string nameText) => {
+
+                    UI_Blocker.Hide_Static();
+                    highscoreTable.AddHighscoreEntry(score, nameText);
+                });
+            });
+        };
+    }
+}
